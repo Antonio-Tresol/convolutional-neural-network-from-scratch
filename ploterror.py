c6daa2f1b270c82ed3eb5c70b9c39649692a1809
Antonio Badilla-Olivas <84429050+Antonio-Tresol@users.noreply.github.com>
2023-11-30 13:53:57 -0600

    Add pie chart for classification error

--- ploterror.py
+++ ploterror.py
@@ -17,6 +17,22 @@ def main():
     plt.ylabel("Error")
     plt.show()
 
+    # pie char for error
+    labels = "misclassified", "correctly classified"
+    file_path = "classification_error_data.csv"
+    file_path = os.path.join(folder_path, file_path)
+    classification_error_data = np.loadtxt(file_path, delimiter=",")
+    # misses are stored as 1, correct as 0
+    misses = np.count_nonzero(classification_error_data)
+    correct = len(classification_error_data) - misses
+    sizes = [correct, misses]
+    explode = (0.1, 0)
+    miss_and_correct_figure, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct="%1.1f%%", shadow=True)
+    ax1.axis("equal")
+    plt.title("Missed and Correctly classified")
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
